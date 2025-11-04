dba44830736f7635238474749646a966ba4b4846
Or Balayla <oribalila@gmail.com>
2025-11-04 11:00:06 +0200

    feat(router/logging): introduce structured logging for the router and the lifecycle of the packet it receives

--- router/router.py
+++ router/router.py
@@ -1,7 +1,12 @@
+import os
 import copy
+import json
+import logging
 import subprocess
+import logging.config
 
 from socket import SOCK_RAW, AF_PACKET, htons, socket
+from pathlib import Path
 from ipaddress import IPv4Address, IPv4Network
 from threading import Thread
 from collections.abc import Generator
@@ -29,11 +34,26 @@ BUFFER_SIZE = 65535
 MAXIMUM_ARP_REQUESTS = 5
 DEFAULT_MTU = 1500
 
+log_dir = "logs"
+directory = Path(__file__).resolve().parent
+os.makedirs(directory / log_dir, exist_ok=True)
+
+with open(directory / "logging_config.json") as logging_config_json:
+    logging_config = json.load(logging_config_json)
+    logging_config["handlers"]["file"]["filename"] = (
+        directory / logging_config["handlers"]["file"]["filename"]
+    )
+    logging.config.dictConfig(logging_config)
+
+
+logger = logging.getLogger(__name__)
+
 
 class Router:
     """This class represents the router."""
 
     def __init__(self, routing_table_path: str | None = None):
+        logger.info("Router initializing", extra={"context": None})
         self.arp_table = ArpTable()
         self.routing_table = RoutingTable(routing_table_path)
         self.myIp = {
@@ -56,15 +76,29 @@ class Router:
             )
             mac = EUI(mac)
             self.interface_mac[interface] = mac
+        logger.debug(
+            "Router initialized",
+            extra={"context": {"interfaces": self.interfaces, "MTU": self.MTU}},
+        )
 
     def capture_packet(self) -> None:
         """Captures packets and sends them for handling."""
         sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))
+        logger.info("Starting packet capture", extra={"context": {"interfaces": self.interfaces}})
         while True:
             bytes_packet, packet_metadata = sock.recvfrom(BUFFER_SIZE)
             interface = packet_metadata[0]
             if interface in self.interfaces:
+                logger.debug("Captured packet", extra={"context": {"interface": interface}})
+                logger.debug(
+                    "Converting packet from bytes to object",
+                    extra={"context": {"packet": bytes_packet, "interface": interface}},
+                )
                 packet = Packet.bytes_constructor(bytes_packet)
+                logger.debug(
+                    "Handling packet in a thread",
+                    extra={"context": {"packet": packet, "interface": interface}},
+                )
                 thread = Thread(target=self.handle_packet, args=(packet, interface))
                 thread.start()
         sock.close()
@@ -77,15 +111,41 @@ class Router:
             (str) interface: The incoming interface
         """
         if packet.layers[0].ether_type == IPv4_CODE:
+            logger.debug(
+                "Packet was classified as IPv4",
+                extra={"context": {"packet": packet, "interface": interface}},
+            )
             source_ip = packet.layers[1].source
             if source_ip in self.myIp.values():
+                logger.debug(
+                    "Stopping packet handling — packet originated from the router",
+                    extra={"context": {"source_ip": source_ip}},
+                )
                 return
+            logger.debug(
+                "Handling IPv4 packet",
+                extra={"context": {"packet": packet, "interface": interface}},
+            )
             self.handle_ipv4(packet, interface)
         elif (
             packet.layers[0].ether_type == ARP_CODE
             and packet.layers[1].target_protocol == self.myIp[interface]
         ):
+            logger.debug(
+                "Packet was classified as an ARP for the router",
+                extra={
+                    "context": {
+                        "ether_type": packet.layers[0].ether_type,
+                        "target_protocol": packet.layers[1].target_protocol,
+                    }
+                },
+            )
             self.handle_arp(packet.layers[1], interface)
+        else:
+            logger.warning(
+                "No handler for frame",
+                extra={"context": {"ether_type": packet.layers[0].ether_type, "packet": packet}},
+            )
 
     def handle_arp(self, arp_header: ARP, interface: str) -> None:
         """Handles incoming ARP packet.
@@ -94,16 +154,25 @@ class Router:
             (Packet) arp_header:    The ARP packet
             (str) interface:        The incoming interface
         """
-        self.arp_table.add(
-            ArpTableEntry(arp_header.sender_hardware, arp_header.sender_protocol, "dynamic")
+        arp_table_entry = ArpTableEntry(
+            arp_header.sender_hardware, arp_header.sender_protocol, "dynamic"
         )
+        logger.debug("Updating ARP table", extra={"context": {"entry": arp_table_entry}})
+        self.arp_table.add(arp_table_entry)
 
         next_hop = self.get_next_hop(arp_header.sender_protocol)
         network = IPv4Network(f"{arp_header.sender_protocol}/32")
-        self.routing_table.add(RoutingTableEntry(network, next_hop, interface))
+        routing_table_entry = RoutingTableEntry(network, next_hop, interface)
+        logger.debug("Updating routing table", extra={"context": {"entry": routing_table_entry}})
+        self.routing_table.add(routing_table_entry)
 
         if arp_header.opcode == REQUEST_OPCODE:
-            self.send_packet(self.build_arp_reply(interface, arp_header.sender_protocol), interface)
+            arp_reply_packet = self.build_arp_reply(interface, arp_header.sender_protocol)
+            logger.debug(
+                "Packet is an ARP request. Sending ARP reply",
+                extra={"context": {"opcode": arp_header.opcode, "packet": arp_reply_packet}},
+            )
+            self.send_packet(arp_reply_packet, interface)
 
     def handle_ipv4(self, packet: Packet, input_interface: str) -> None:
         """Handles incoming IPv4 packet.
@@ -120,6 +189,10 @@ class Router:
         while (
             ip_header.source not in self.arp_table and arp_requests_counter < MAXIMUM_ARP_REQUESTS
         ):
+            logger.debug(
+                "Sender IP is not in the ARP table. Sending an ARP request",
+                extra={"context": {"source_ip": ip_header.source}},
+            )
             self.send_packet(
                 self.build_arp_request(input_interface, ip_header.source),
                 input_interface,
@@ -132,12 +205,25 @@ class Router:
             ip_header.destination not in self.arp_table
             and arp_requests_counter < MAXIMUM_ARP_REQUESTS
         ):
+            logger.debug(
+                "Destination IP is not in the ARP table. Sending an ARP request",
+                extra={"context": {"destination_ip": ip_header.destination}},
+            )
             self.send_packet(
                 self.build_arp_request(output_interface, ip_header.destination),
                 output_interface,
             )
             arp_requests_counter += 1
         if arp_requests_counter == MAXIMUM_ARP_REQUESTS:
+            logger.warning(
+                "Sent too much ARP requests without finding target MAC address. Aborting",
+                extra={
+                    "context": {
+                        "arp_requests_counter": arp_requests_counter,
+                        "output_interface": output_interface,
+                    }
+                },
+            )
             return
 
         if (
@@ -145,9 +231,20 @@ class Router:
             and ipv4_payload.icmp_type == protocols.constants.icmp_constants.REQUEST_TYPE
             and ip_header.destination in self.myIp.values()
         ):
+            logger.info(
+                "Identified packet as an ICMP echo request for the router",
+                extra={"context": {"destination_ip": ip_header.destination}},
+            )
+            icmp_reply = self.build_icmp_reply(
+                ipv4_payload, ip_header.source, ip_header.destination
+            )
             self.send_packet(
-                self.build_icmp_reply(ipv4_payload, ip_header.source, ip_header.destination),
+                icmp_reply,
                 input_interface,
+            )
+            logger.info(
+                "Sending an ICMP echo reply",
+                extra={"context": {"destination_ip": icmp_reply.layers[1].destination}},
             )
             return
 
@@ -164,11 +261,16 @@ class Router:
         Parameters:
             (Packet) packet: The packet to forward
         """
+        logger.info("Forwarding packet", extra={"context": {"packet": packet}})
         ethernet_header = packet.layers[0]
         ip_header = packet.layers[1]
         ipv4_payload = packet.layers[2]
 
         interface = self.routing_table.find_interface(ip_header.destination)
+        logger.debug(
+            "Found packet destination interface for forwarding",
+            extra={"context": {"interface": interface}},
+        )
 
         new_destination_mac = self.arp_table[ip_header.destination]
         new_source_mac = self.interface_mac[interface]
@@ -178,7 +280,11 @@ class Router:
         new_ipv4_header = copy.copy(ip_header)
         new_ipv4_header.ttl -= 1
         packet = Packet([new_ethernet_header, new_ipv4_header, ipv4_payload])
+        logger.debug("Altered packet", extra={"context": {"altered_packet": packet}})
 
+        logger.debug(
+            "Forwarding packet", extra={"context": {"packet": packet, "interface": interface}}
+        )
         self.send_packet(packet, interface)
 
     def get_next_hop(self, dest_ip: IPv4Address) -> IPv4Address:
@@ -204,6 +310,9 @@ class Router:
         Return:
             (generator): A generator of the fragments
         """
+        logger.info(
+            "Fragmenting packet", extra={"context": {"packet": packet, "packet_size": len(packet)}}
+        )
         if not isinstance(packet.layers[1], IPv4):
             raise TypeError("Not an IPv4 packet")
 
@@ -230,6 +339,7 @@ class Router:
             new_ipv4_header.fragment_offset = start
 
             fragment = bytes(new_ipv4_header) + fragment_data
+            logger.debug("Fragment was created", extra={"context": {"fragment": fragment}})
             yield fragment
 
     def build_arp_request(self, interface: str, target_ip: IPv4Address):
@@ -339,5 +449,6 @@ def send_bytes_packet(packet: bytes, interface: str) -> None:
     """
     sock = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL))
     sock.bind((interface, 0))
+    logger.debug("Sending packet", extra={"context": {"packet": packet, "interface": interface}})
     sock.send(packet)
     sock.close()
